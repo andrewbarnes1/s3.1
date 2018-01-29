335db1029acf548ba5bf50d74123c93bcfbeecaa
Student <student@C17775444-lnx.offcampusnetwork.co.uk>
2018-01-29 16:28:51 +0000

    Changed the code to display the read voltage. Includes calculations

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,14 @@
 int main() {
     AnalogIn  left(A0);
     AnalogIn right(A1);
+
+    // Prints out the voltage from the potentiometers every second.
     while(1) {
-        printf(" Left dial %f \n",  left.read() );
-        printf("Right dial %f \n", right.read() );
+        // Output is scaled between 0 - 1 (1 = 3.3v)
+        printf(" Left dial %f \n",  (left.read() * 3.3) );
+        printf("Right dial %f \n", (right.read() * 3.3) );
         wait( 1.0 );
-    }
+        // Voltage Change = 3.3 / 2**16
+        // Angle = 360 / 2**16
+        }
 }
